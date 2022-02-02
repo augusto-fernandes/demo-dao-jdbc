985dc4e0f8dd47d102ec982793917bbf5b28e8c4
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-02-01 21:53:05 -0300

    classe departamento

---
+++ src/application/programm.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class programm {
+
+	public static void main(String[] args) {
+		Department obj = new Department(1, "books");
+		System.out.println(obj);
+
+	}
+
+}
