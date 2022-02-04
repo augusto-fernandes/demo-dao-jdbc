899be3b76386c5accd9444c57243d2270752e3b2
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-02-03 21:23:24 -0300

    reusando a instanciação

--- src/application/programm.java
+++ src/application/programm.java
@@ -9,6 +9,7 @@ public class programm {
 	public static void main(String[] args) {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 				
+		System.out.println("=== TESTE 1 : seller findByID ====");
 		Seller seller = sellerDao.findByID(3);
 		
 		System.out.println(seller);
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -51,16 +51,8 @@ public class SellerDaoJDBC implements SellerDao{
 			st.setInt(1, id);
 			rs = st.executeQuery();
 			if(rs.next()) {
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getString("DepName"));
-				Seller obj = new Seller();
-				obj.setId(rs.getInt("Id"));
-				obj.setName(rs.getString("Name"));
-				obj.setEmail(rs.getString("Email"));
-				obj.setBaseSalary(rs.getDouble("BaseSalary"));
-				obj.setBirthDate(rs.getDate("BirthDate"));
-				obj.setDepartment(dep);
+				Department dep = instantietDepartment(rs);
+				Seller obj =  instatietSeller(rs, dep);
 				return obj;
 			}
 			return null;
@@ -74,6 +66,22 @@ public class SellerDaoJDBC implements SellerDao{
 		}
 	}
 
+	private Seller instatietSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller obj =new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep);
+		return obj;
+	}
+	private Department instantietDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
+	}
 	@Override
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
