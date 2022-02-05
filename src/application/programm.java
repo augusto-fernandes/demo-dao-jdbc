5d28ba19cc2e19e5746312239cbbf2b84dc45445
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-02-04 22:14:58 -0300

    insert implementado

--- src/application/programm.java
+++ src/application/programm.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -27,6 +28,11 @@ public class programm {
 			list = sellerDao.findAll();
 			for (Seller obj1 : list) {
 				System.out.println(obj1);
+			
+				System.out.println("\n=== TEST 4: seller insert =====");
+				Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+				sellerDao.insert(newSeller);
+				System.out.println("Inserted! New id = " + newSeller.getId());
 			}
 
 		}
